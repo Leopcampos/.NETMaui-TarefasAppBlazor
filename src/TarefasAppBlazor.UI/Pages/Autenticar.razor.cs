d7a7fa91f955fa2393b69aa73e015ace7ba3a0b3
Leo Campos <leopcampos85@gmail.com>
2023-09-18 19:39:00 -0300

    Criação dos métodos na classe Autenticar.razor.cs, alterando o NavMenu para ocultar os métodos sem estar logado e trabalhando com o Blazored.LocalStorage.

--- src/TarefasAppBlazor.UI/Pages/Autenticar.razor.cs
+++ src/TarefasAppBlazor.UI/Pages/Autenticar.razor.cs
@@ -1,11 +1,19 @@
+using Microsoft.AspNetCore.Components;
 using TarefasAppBlazor.Services.Helpers;
 using TarefasAppBlazor.Services.Models.Requests;
 using TarefasAppBlazor.Services.Models.Responses;
+using TarefasAppBlazor.UI.Helpers;
 
 namespace TarefasAppBlazor.UI.Pages
 {
     public partial class Autenticar
     {
+        [Inject]
+        AuthenticationHelper? AuthenticationHelper { get; set; }
+
+        [Inject]
+        NavigationManager? NavigationManager { get; set; }
+
         //Definindo o modelo de dados do componente
         private AutenticarRequestModel model = new AutenticarRequestModel();
 
@@ -28,9 +36,14 @@ namespace TarefasAppBlazor.UI.Pages
                 //fazendo a requisição para o serviço de cadastro da API
                 var servicesHelper = new ServicesHelper();
                 var result = await servicesHelper.Post<AutenticarRequestModel, AutenticarResponseModel>("autenticar", model);
-
                 mensagemSucesso = $"Parabéns {result.Nome}, autenticação realizada com sucesso!";
                 model = new AutenticarRequestModel();
+
+                //Salvar os dados do usuário autenticado
+                await AuthenticationHelper.SignIn(result);
+
+                //Navegar para a página do dashboard
+                NavigationManager.NavigateTo("/app/dashboard", true);
             }
             catch (Exception e)
             {
